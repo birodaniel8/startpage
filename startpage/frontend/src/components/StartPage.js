b036a9038d5b49c23ca28bcc1473a41e7d3c4632
birodaniel8 <birodaniel8@gmail.com>
2021-04-22 18:33:56 +0200

    app title and check added

--- startpage/frontend/src/components/StartPage.js
+++ startpage/frontend/src/components/StartPage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Grid, Typography, FormControlLabel, Collapse, Switch, Card, CardContent, IconButton, 
+  CardMedia } from "@material-ui/core";
 
 const StartPage = () => {
+  const [newCityFormChecked, setNewCityFormChecked] = useState(false);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          Weather App
+        </Typography>
+        <FormControlLabel
+          control={<Switch checked={newCityFormChecked} 
+          onChange={() => setNewCityFormChecked(!newCityFormChecked)} 
+          name="newCityForm" />}
+          label="Show new city form"
+        />
+        <Typography component="h4" variant="h4">{newCityFormChecked.toString()}</Typography>
+      </Grid>
+    </Grid>
   );
 }
 
